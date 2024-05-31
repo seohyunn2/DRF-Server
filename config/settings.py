9dd1b7d1fc99b3fd66cf17fb98f1f18ed1f16dad
Seohyun <tjgus9138@naver.com>
2024-05-31 21:13:42 +0900

    settings

--- config/settings.py
+++ config/settings.py
@@ -33,17 +33,19 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'http://127.0.0.1:8000',
     '127.0.0.1',
+    'port-0-drf-server-ss7z32llwum4kin.sel5.cloudtype.app',
 ]
 # CORS 설정
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
     "http://127.0.0.1:8000",
+    'port-0-drf-server-ss7z32llwum4kin.sel5.cloudtype.app',
 ]
 # CSRF 설정
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
+    'port-0-drf-server-ss7z32llwum4kin.sel5.cloudtype.app',
 ]
 
 # Application definition
